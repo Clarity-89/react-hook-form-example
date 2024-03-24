3bb51cef528d6079292a0588790ab26cc200c4f2
Clarity-89 <homes89@ukr.net>
2024-03-24 10:49:18 +0200

    Fix import

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
-import App from "./App.js";
+import App from "./App";
 
 const root = document.getElementById("root");
 if (root === null) {
